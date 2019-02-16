116e434dc48ec30bc008ff9593964e42016f2cdd
Alex Sandro A. Souza <asasouza.alex@gmail.com>
2019-02-16 17:21:05 -0200

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,86 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const path = require('path');
 
 const htmlWebpackPlugin = new HtmlWebPackPlugin({
-	template: './index.html',
-	filename: './index.html',
-  // favicon: './path/to/icon',
-	minify: {
-		collapseWhitespace: true
-	},
+  template: './index.html',
+  filename: './index.html',
+  minify: {
+    collapseWhitespace: true
+  },
   chunksSortMode: 'none',
-  // inject: 'head'
 });
+
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve('dist'),
-		filename: '[name].bundle.js',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: '[name].bundle.js',
     chunkFilename: '[name].bundle.js',
-	},
-	module: {
-		rules: [
-		{ test: /\.js$/, exclude: /node_modules/, use: { loader: 'babel-loader' } },
-		{ test: /\.css$/, 
-			use: [
-			{
-				loader: 'style-loader'
-			},
-			{
-				loader: 'css-loader',
-			}
-			]
-		},
-		{ test: /\.(png|jpg|gif)$/, use: [{ loader: 'file-loader', options: { outputPath: 'resources/images/' } }] },
-      { test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-      	use: [{
-      		loader: 'file-loader',
-      		options: {
-      			name: '[name].[ext]',
-      			outputPath: 'resources/fonts/'
-      		}
-      	}]
+  },
+  module: {
+    rules: [
+    { test: /\.js$/, exclude: /node_modules/, use: { loader: 'babel-loader' } },
+    { test: /\.css$/, 
+      use: [
+      {
+        loader: 'style-loader'
       },
       {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader'
-          },
-          {
-            loader: 'sass-loader'
-          }
-        ]
+        loader: 'css-loader',
       }
       ]
+    },
+    { test: /\.(png|jpg|gif)$/, use: [{ loader: 'file-loader', options: { outputPath: 'resources/images/' } }] },
+    { test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'resources/fonts/'
+        }
+      }]
+    },
+    {
+      test: /\.scss$/,
+      use: [
+      {
+        loader: 'style-loader'
+      },
+      {
+        loader: 'css-loader'
+      },
+      {
+        loader: 'sass-loader'
+      }
+      ]
+    }
+    ]
   },
   plugins: [
-    htmlWebpackPlugin,
-  	],
-  	devServer: {
-  		historyApiFallback: true
-  	},
-  	resolve: {
-  		alias: {
-        assets: path.resolve(__dirname, './src/assets/'),
-  			components: path.resolve(__dirname, './src/components/'),
-        constants: path.resolve(__dirname, './src/constants/'),
-        ducks: path.resolve(__dirname, './src/ducks/'),
-        providers: path.resolve(__dirname, './src/providers/'),
-        routers: path.resolve(__dirname, './src/routers'),
-        views: path.resolve(__dirname, './src/views/')
+  htmlWebpackPlugin,
+  ],
+  devServer: {
+    historyApiFallback: true
+  },
+  resolve: {
+    alias: {
+      assets: path.resolve(__dirname, './src/assets/'),
+      components: path.resolve(__dirname, './src/components/'),
+      constants: path.resolve(__dirname, './src/constants/'),
+      ducks: path.resolve(__dirname, './src/ducks/'),
+      providers: path.resolve(__dirname, './src/providers/'),
+      routers: path.resolve(__dirname, './src/routers'),
+      views: path.resolve(__dirname, './src/views/')
 
-  		}
-  	},
-  };
+    }
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: true,
+      })
+      ],
+  },
+};
